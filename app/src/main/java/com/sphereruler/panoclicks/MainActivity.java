d52afc741ecceacc5cb683ae9b43caf0eabb6fa5
Sahil-Ansari-99 <39992186+Sahil-Ansari-99@users.noreply.github.com>
2019-03-02 23:46:16 +0530

    added home screen.

--- app/src/main/java/com/sphereruler/panoclicks/MainActivity.java
+++ app/src/main/java/com/sphereruler/panoclicks/MainActivity.java
@@ -1,13 +1,20 @@
 package com.sphereruler.panoclicks;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    FloatingActionButton cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView=(RecyclerView)findViewById(R.id.home_recyclerView);
+        cameraButton=(FloatingActionButton)findViewById(R.id.home_cameraButton);
     }
 }
