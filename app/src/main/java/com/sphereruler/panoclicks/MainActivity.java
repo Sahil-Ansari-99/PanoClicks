d44a980a143c79c714c38706eb033ba7c71f54e3
Sahil-Ansari-99 <39992186+Sahil-Ansari-99@users.noreply.github.com>
2019-03-03 00:46:10 +0530

    added camera.

--- app/src/main/java/com/sphereruler/panoclicks/MainActivity.java
+++ app/src/main/java/com/sphereruler/panoclicks/MainActivity.java
@@ -1,13 +1,26 @@
 package com.sphereruler.panoclicks;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     FloatingActionButton cameraButton;
+    private static int CAMERA_REQUEST_CODE=1888;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +29,43 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView=(RecyclerView)findViewById(R.id.home_recyclerView);
         cameraButton=(FloatingActionButton)findViewById(R.id.home_cameraButton);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openCamera();
+            }
+        });
+    }
+
+    public void openCamera(){
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==CAMERA_REQUEST_CODE){
+            try {
+                Bitmap pic = (Bitmap) data.getExtras().get("data");
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String fileName = "JPEG_" + timeStamp + "_";
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                try {
+                    File image = File.createTempFile(fileName, ".jpg", storageDir);
+                    FileOutputStream fos = new FileOutputStream(image);
+                    pic.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    fos.close();
+                    Toast.makeText(getApplicationContext(), "Image saved in" + storageDir.toString(), Toast.LENGTH_SHORT).show();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
