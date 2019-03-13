c235544a4de64e97cecf59897997b086c1b61835
Sahil-Ansari-99 <39992186+Sahil-Ansari-99@users.noreply.github.com>
2019-03-14 01:17:54 +0530

    added camera

--- app/src/main/java/com/sphereruler/panoclicks/CameraActivity.java
+++ app/src/main/java/com/sphereruler/panoclicks/CameraActivity.java
@@ -3,11 +3,16 @@ package com.sphereruler.panoclicks;
 import android.app.Activity;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 public class CameraActivity extends Activity {
     private Camera camera;
     private CameraPreview cameraPreview;
+    private FloatingActionButton cameraButton;
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
@@ -15,9 +20,19 @@ public class CameraActivity extends Activity {
         setContentView(R.layout.activity_camera);
 
         camera=getCameraInstance();
+        camera.setDisplayOrientation(90);
         cameraPreview=new CameraPreview(this, camera);
         FrameLayout frameLayout=(FrameLayout)findViewById(R.id.camera_preview);
         frameLayout.addView(cameraPreview);
+
+        cameraButton=(FloatingActionButton)findViewById(R.id.button_capture);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public static Camera getCameraInstance(){
